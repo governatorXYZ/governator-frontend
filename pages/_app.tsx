c3b1b415aa2141bba83ede6be52dd21368b5d2e7
kenlok <ken.lok@eezee.com.sg>
2022-03-03 23:29:50 +0800

    (chore): add next auth session provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react'
+import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import theme from 'config/theme'
 import NavBar from 'components/NavBar'
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <NavBar />
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <NavBar />
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
   )
 }
 
