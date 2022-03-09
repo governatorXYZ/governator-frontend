9966bee607c9de078b514d2b244e96a700ff415f
kenlok <ken.lok@eezee.com.sg>
2022-03-09 16:54:36 +0800

    (chroe): refactor breadcrumbs

--- pages/polls/create.tsx
+++ pages/polls/create.tsx
@@ -1,38 +1,18 @@
 import type { NextPage } from 'next'
 import {
   Box,
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
   Text,
   Flex,
 } from '@chakra-ui/react'
-import { ChevronRightIcon } from '@chakra-ui/icons'
 import { BiBarChartSquare } from 'react-icons/bi'
 import CreatePollForm from 'components/polls/CreatePollForm'
+import GovCrumb from 'components/BreadCrumb';
 
 const CreatePoll: NextPage = () => {
   return (
     <Box bg='dark-2' minH='calc(100vh - 90px)' pt='4rem' pb='8rem'>
       <Box bg='dark-1' maxW='2xl' mx='auto' p='2rem 3rem'>
-        <Breadcrumb
-          spacing='8px'
-          separator={<ChevronRightIcon color='gray.500' />}
-          color='gray.300'
-        >
-          <BreadcrumbItem>
-            <BreadcrumbLink href='#'>Servers</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <BreadcrumbLink href='#'>BanklessDAO</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem>
-            <BreadcrumbLink href='#'>Polls</BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbItem isCurrentPage color='blue.300' fontWeight='500'>
-            <BreadcrumbLink href='#'>Create</BreadcrumbLink>
-          </BreadcrumbItem>
-        </Breadcrumb>
+        <GovCrumb />
         <Flex
           color='gray.100'
           fontSize='2xl'
