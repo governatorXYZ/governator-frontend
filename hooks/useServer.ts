225a73eb73e9c6ed3ea48c28602ad54e79ca1f98
tikisailor <40813246+tikisailor@users.noreply.github.com>
2023-01-08 17:55:03 -0700

    remove hardcoded channel filter since filtering done by backend now

--- hooks/useServer.ts
+++ hooks/useServer.ts
@@ -9,15 +9,6 @@ import { useState, useCallback, useEffect } from 'react'
 import { useSession } from 'next-auth/react'
 import useServers from './useServers'
 
-/**
- * @important to remove in the future
- * hardcode BOT GARAGE's - govbot-testing channel id as allowed
- * included the channel name for readability in case we have more to whitelist
- */
-const MVP_ALLOWED_CHANNELS = {
-  "govbot-testing": "959216625180098590"
-};
-
 
 const useServer = () => {
   const [loading, setLoading] = useState(false)
@@ -37,19 +28,12 @@ const useServer = () => {
           `/client/discord/${currentServer.id}/channels/${session?.discordId}`
         )
 
-        // const channelsResponse = {
-        //   data: channels_response
-        // };
-
         const sortedChannels = (
           channelsResponse?.data?.data as Record<number, string>[]
         )
           ?.map(c => {
             const entries = Object.entries(c)[0]
             return { value: entries[0], label: entries[1] }
-          })
-          .filter(c => {
-            return Object.values(MVP_ALLOWED_CHANNELS).includes(c.value)
           })
           .sort((curr, next) =>
             curr.label.toLowerCase() < next.label.toLowerCase() ? -1 : 1
@@ -60,10 +44,6 @@ const useServer = () => {
         const rolesResponse = await privateBaseFetcher(
           `/client/discord/${currentServer.id}/roles/${session?.discordId}`
         )
-
-        // const rolesResponse = {
-        //   data: roles_response
-        // };
 
         const sortedRoles = (
           rolesResponse?.data?.data as Record<number, string>[]
