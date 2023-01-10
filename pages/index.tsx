b133d7724b16617209de57a2d59907bd3082c3c5
Zach Berwaldt <zberwaldt@tutamail.com>
2023-01-09 19:58:37 -0500

    add new hero buttons, back link update

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ import {
 } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import SVGWall from 'components/SVGWall'
-import { FaDiscord } from 'react-icons/fa'
+import { FaChartBar, FaDiscord, FaSignInAlt } from 'react-icons/fa'
 import { SiNotion } from 'react-icons/si'
 import { FiBarChart, FiGlobe, FiMessageSquare, FiTwitter } from 'react-icons/fi'
 import { AiOutlineMedium, AiOutlineTrophy } from 'react-icons/ai'
@@ -21,6 +21,7 @@ import styled from '@emotion/styled'
 import { useEffect, useState } from 'react'
 import { sample } from 'lodash'
 import Link from 'next/link'
+import { useSession } from 'next-auth/react'
 
 const StyledBox = styled(Box)`
   background-color: #29303a;
@@ -60,6 +61,12 @@ const Quote: React.FC = () => {
 
   return <Box>&quot;{sentence}&quot;</Box>
 }
+
+const HeroButton = () => {
+  const { data: session } = useSession();
+  return session ? (<Button color='gray.700' leftIcon={<FaChartBar />}>Dashboard</Button>) :
+  (<Button color='gray.700' leftIcon={<FaSignInAlt />}>Login</Button>);
+};
 
 const Home: NextPage = waitlistDisabled => {
   return (
@@ -190,9 +197,7 @@ const Home: NextPage = waitlistDisabled => {
                           </Text>
                         </Button>
                     ) : (
-                        <Button color='gray.700' leftIcon={<SiNotion />}>
-                          Read docs on Notion
-                        </Button>
+                        <HeroButton />
                     )}
                   </Flex>
                 </motion.div>
