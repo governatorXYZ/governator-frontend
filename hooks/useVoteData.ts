0f8ccf83702a558bfad8629b1c31ead47ae6beca
Zach Berwaldt <zberwaldt@tutamail.com>
2023-02-18 14:56:03 -0500

    add some types from sdk to verify process

--- hooks/useVoteData.ts
+++ hooks/useVoteData.ts
@@ -1,14 +1,23 @@
+import { AxiosResponse } from "axios";
 import useSWR from "swr";
 import {privateBaseFetcher} from "../constants/axios";
 
 export const useVotesData = (pollId: string) => {
-    const { data } = useSWR(`/vote/results/sum/${pollId}`, privateBaseFetcher)
+    const { data } = useSWR<AxiosResponse<{
+            data: { 
+                aggregate: Array<unknown>;
+                votes: Array<unknown>;
+            }
+        }
+    >>(`/vote/results/sum/${pollId}`, privateBaseFetcher)
     const votesData = data?.data ? data?.data : []
     return { votesData }
 }
 
 export const useTotalVotes = (pollId: string) => {
-    const { data } = useSWR(`/vote/results/votes-per-user/count/${pollId}`, privateBaseFetcher)
+    const { data } = useSWR<AxiosResponse<{
+        data: string;
+    }>>(`/vote/results/votes-per-user/count/${pollId}`, privateBaseFetcher)
     const totalVotes = data?.data ? data?.data : '0'
     return { totalVotes }
 }
